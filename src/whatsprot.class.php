15f474199ac5b42b16ee9906abcd1be758e64913
mgp25 <me@mgp25.com>
2014-10-22 01:00:16 +0200

    Added the sendGetBroadcastLists function

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -883,6 +883,22 @@ class WhatsProt
 			), array($pricingNode), null);
 		$this->sendNode($node);
 	}
+
+  /**
+	* Gets all the broadcast lists for an account
+	*/
+	public function sendGetBroadcastLists()
+	{
+		$msgId = $this->createMsgId("get_lists_");
+		$listsNode = new ProtocolNode("lists", null, null, null);
+		$node = new ProtocolNode("iq", array(
+			"id" => $msgId,
+			"xmlns" => "w:b",
+			"type" => "get",
+			"to" => "s.whatsapp.net"
+			), array($listsNode), null);
+		$this->sendNode($node);
+	}
 	
 	/**
 	* Send a request to get the normalized mobile number respresenting the JID
