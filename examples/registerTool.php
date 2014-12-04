fc043660f3dbdf17e7ff247718c754fe052ec667
mgp25 <me@mgp25.com>
2014-12-04 20:11:10 +0100

    Update registerTool
    Now supports carriers

--- examples/registerTool.php
+++ examples/registerTool.php
@@ -12,9 +12,11 @@ echo "####################\n";
 
 echo "\n\nUsername (country code + number without + or 00): ";
 $username = trim(fgets(STDIN));
-$identity = "";
+$identity = "identity";
 echo "\nNickname: ";
 $nickname = trim(fgets(STDIN));
+echo "\nCarrier: ";
+$carrier = trim(fgets(STDIN));
 
 // Create a instance of WhastPort.
 $w = new WhatsProt($username, $identity, $nickname, $debug);
@@ -22,20 +24,26 @@ $w = new WhatsProt($username, $identity, $nickname, $debug);
 echo "\n\nType sms or voice: ";
 $option = fgets(STDIN);
 
-try {
-    $w->codeRequest(trim($option));
-} catch (Exception $e) {
-    echo "\nError: $e";
-    exit(0);
+try
+{
+  $w->codeRequest(trim($option), $carrier);
+}
+catch(Exception $e)
+{
+  echo $e->getMessage();
+  exit(0);
 }
 
 echo "\n\nEnter the received code: ";
 $code = fgets(STDIN);
 
-try {
-    $result = $w->codeRegister(trim($code));
-    echo "\nYour password is: " . $result->pw;
-} catch (Exception $e) {
-    echo "\Error: $e";
-    exit(0);
+try
+{
+  $result = $w->codeRegister(trim($code));
+  echo "\nYour password is: ".$result->pw."\n";
+}
+catch(Exception $e)
+{
+  echo $e->getMessage();
+  exit(0);
 }
