40279e8b5067d8bf18b4d7ee965b3cb0a9308df0
lucamemini <luca@memini.it>
2015-02-04 16:08:16 +0100

    Update whatsprot.class.php

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -2610,22 +2610,22 @@ class WhatsProt
 
 		}
         elseif ($node->getTag() == "success") {
-	    if ($node->getAttribute("status") == "active") {
+	    	if ($node->getAttribute("status") == "active") {
 	            $this->loginStatus = static::CONNECTED_STATUS;
 	            $challengeData = $node->getData();
 	            file_put_contents($this->challengeFilename, $challengeData);
 	            $this->writer->setKey($this->outputKey);
-	   } elseif ($node->getAttribute("status") == "expired") 
-	   {
-            $this->eventManager()->fire("onAccountExpired",
-                array(
-                    $this->phoneNumber,
-                    $node->getAttribute("kind"),
-                    $node->getAttribute("status"),
-                    $node->getAttribute("creation"),
-                    $node->getAttribute("expiration")
-                ));
-	   }
+	   		} elseif ($node->getAttribute("status") == "expired") 
+	   		{
+            	$this->eventManager()->fire("onAccountExpired",
+                	array(
+                    	$this->phoneNumber,
+                    	$node->getAttribute("kind"),
+                    	$node->getAttribute("status"),
+                    	$node->getAttribute("creation"),
+                    	$node->getAttribute("expiration")
+                	));
+	    	}
         } elseif($node->getTag() == "failure")
         {
             $this->eventManager()->fire("onLoginFailed",
