5823e3531a452fa1a3318fce45db47411acd7bb5
ducch <ducch@ducch.info>
2015-02-24 17:47:49 +0200

    put next challenge files in a cache folder (#225)

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -36,6 +36,7 @@ class WhatsProt
     const DISCONNECTED_STATUS = 'disconnected';                                              // Describes the connection status with the WhatsApp server.
     const MEDIA_FOLDER = 'media';                                                            // The relative folder to store received media files
     const PICTURES_FOLDER = 'pictures';                                                      // The relative folder to store picture files
+    const CACHE_FOLDER = 'cache';                                                            // The relative folder to store cache files.
     const PORT = 443;                                                                        // The port of the WhatsApp server.
     const TIMEOUT_SEC = 2;                                                                   // The timeout for the connection with the WhatsApp servers.
     const TIMEOUT_USEC = 0;
@@ -55,7 +56,7 @@ class WhatsProt
      * Property declarations.
      */
     protected $accountInfo;             // The AccountInfo object.
-    protected $challengeFilename = 'nextChallenge.dat';
+    protected $challengeFilename;       // Path to nextChallenge.dat.
     protected $challengeData;           //
     protected $debug;                   // Determines whether debug mode is on or off.
     protected $event;                   // An instance of the WhatsApiEvent Manager.
@@ -108,6 +109,12 @@ class WhatsProt
             //use provided identity hash
             $this->identity = urldecode(file_get_contents($identity.'.dat'));
         }
+        //e.g. ./cache/nextChallenge.12125557788.dat
+        $this->challengeFilename = sprintf('%s%s.nextChallenge.%s.dat',
+            self::CACHE_FOLDER,
+            DIRECTORY_SEPARATOR,
+            $number);
+
         $this->name         = $nickname;
         $this->loginStatus  = static::DISCONNECTED_STATUS;
         $this->eventManager = new WhatsApiEventsManager();
