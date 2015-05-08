5cd74a67cbea288bb6f557527a9401cc89cd6bcc
ducch <ducch@ducch.info>
2015-05-08 15:42:44 +0300

    update phpdoc

--- src/whatsprot.class.php
+++ src/whatsprot.class.php
@@ -74,7 +74,7 @@ class WhatsProt
      *   The user name.
      * @param $debug
      *   Debug on or off, false by default.
-     * @param string $identityFile
+     * @param mixed $identityFile
      *  Path to identity file, overrides default path
      */
     public function __construct($number, $nickname, $debug = false, $identityFile = false)
@@ -1504,8 +1504,8 @@ class WhatsProt
     /**
      * Send a read receipt to a message.
      *
-     * @param $to The recipient.
-     * @param $id
+     * @param string $to The recipient.
+     * @param string $id
      */
     public function sendMessageRead($to, $id)
     {
@@ -2248,7 +2248,7 @@ class WhatsProt
     /**
      * Create an identity string
      *
-     * @param  string $identity_file IdentityFile (optional).
+     * @param  mixed $identity_file IdentityFile (optional).
      * @return string           Correctly formatted identity
      *
      * @throws Exception        Error when cannot write identity data to file.
@@ -3399,7 +3399,8 @@ class WhatsProt
     }
 
     /**
-     * @param $node ProtocolNode
+     * @param $node  ProtocolNode
+     * @param $class string
      */
     protected function sendAck($node, $class)
     {
@@ -3851,6 +3852,7 @@ class WhatsProt
      * @param ProtocolNode $node The ProtocolTreeNode that contains the message.
      * @param string       $type
      * @param string       $participant
+     * @param string       $callId
      */
     protected function sendReceipt($node, $type = "read", $participant = null, $callId = null)
     {
@@ -3986,6 +3988,7 @@ class WhatsProt
 
     /**
      * @param ProtocolNode $groupNode
+     * @param string       $fromGetGroups
      */
     protected function handleGroupV2InfoResponse(ProtocolNode $groupNode, $fromGetGroups = false)
     {
