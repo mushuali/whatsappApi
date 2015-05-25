62b23555ff3477cae73ff63416be43bc4c71ab5b
mgp25 <me@mgp25.com>
2015-05-25 21:40:33 +0200

    Fixes #754

--- examples/exampleFunctional.php
+++ examples/exampleFunctional.php
@@ -159,21 +159,23 @@ class ProcessNode
     {
         // Example of process function, you have to guess a number (psss it's 5)
         // If you guess it right you get a gift
-        $text = $node->getChild('body');
-        $text = $text->getData();
-        if ($text && ($text == "5" || trim($text) == "5")) {
-            $this->wp->sendMessageImage($this->target, "https://s3.amazonaws.com/f.cl.ly/items/2F3U0A1K2o051q1q1e1G/baby-nailed-it.jpg");
-            $this->wp->sendMessage($this->target, "Congratulations you guessed the right number!");
-        }
-        elseif (ctype_digit($text)) {
-            if ((int)$text != "5")
-                $this->wp->sendMessage($this->target, "I'm sorry, try again!");
-        }
-        $text = $node->getChild('body');
-        $text = $text->getData();
-        $notify = $node->getAttribute("notify");
+        if ($node->getAttribute("type") == 'text')
+        {
+            $text = $node->getChild('body');
+            $text = $text->getData();
+            if ($text && ($text == "5" || trim($text) == "5")) {
+                $this->wp->sendMessageImage($this->target, "https://s3.amazonaws.com/f.cl.ly/items/2F3U0A1K2o051q1q1e1G/baby-nailed-it.jpg");
+                $this->wp->sendMessage($this->target, "Congratulations you guessed the right number!");
+            }
+            elseif (ctype_digit($text)) {
+              if ((int)$text != "5")
+                  $this->wp->sendMessage($this->target, "I'm sorry, try again!");
+            }
+            $text = $node->getChild('body');
+            $text = $text->getData();
+            $notify = $node->getAttribute("notify");
 
-        echo "\n- ".$notify.": ".$text."    ".date('H:i')."\n";
-
+            echo "\n- ".$notify.": ".$text."    ".date('H:i')."\n";
+        }
     }
 }
