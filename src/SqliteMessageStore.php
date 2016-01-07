69c0d0e378b7e0af2d00c8f1391dbe6cdd00f9d9
WHAnonymous <whanonymous@mail2tor.com>
2016-01-07 08:53:55 -0500

    Applied fixes from StyleCI

--- src/SqliteMessageStore.php
+++ src/SqliteMessageStore.php
@@ -1,4 +1,5 @@
 <?php
+
 interface MessageStoreInterface
 {
     public function saveMessage($from, $to, $txt, $id, $t);
@@ -12,20 +13,18 @@ class SqliteMessageStore implements MessageStoreInterface
 
     public function __construct($number)
     {
-        $fileName = __DIR__ . DIRECTORY_SEPARATOR . self::DATA_FOLDER . DIRECTORY_SEPARATOR . 'msgstore-'.$number.'.db';
+        $fileName = __DIR__.DIRECTORY_SEPARATOR.self::DATA_FOLDER.DIRECTORY_SEPARATOR.'msgstore-'.$number.'.db';
         $createTable = !file_exists($fileName);
 
-        $this->db = new \PDO("sqlite:" . $fileName, null, null, array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-        if ($createTable)
-        {
+        $this->db = new \PDO('sqlite:'.$fileName, null, null, [PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+        if ($createTable) {
             $this->db->exec('CREATE TABLE messages (`from` TEXT, `to` TEXT, message TEXT, id TEXT, t TEXT)');
             $this->db->exec('CREATE TABLE messages_pending(`id` TEXT PRIMARY KEY,`jid` TEXT, `pending` TINYINT(1) DEFAULT 0)');
-        }
-        else{
+        } else {
             //backward compatibility
             $result = $this->db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='messages_pending';")->fetchAll();
-            if($result == null || $result == false || count($result) == 0){
-                 $this->db->exec('CREATE TABLE messages_pending(`id` TEXT PRIMARY KEY,`jid` TEXT, `pending` TINYINT(1) DEFAULT 0)');
+            if ($result == null || $result == false || count($result) == 0) {
+                $this->db->exec('CREATE TABLE messages_pending(`id` TEXT PRIMARY KEY,`jid` TEXT, `pending` TINYINT(1) DEFAULT 0)');
             }
         }
     }
@@ -36,68 +35,68 @@ class SqliteMessageStore implements MessageStoreInterface
         $query = $this->db->prepare($sql);
 
         $query->execute(
-            array(
-                ':from' => $from,
-                ':to' => $to,
-                ':message' => $txt,
+            [
+                ':from'      => $from,
+                ':to'        => $to,
+                ':message'   => $txt,
                 ':messageId' => $id,
-                ':t' => $t
-            )
+                ':t'         => $t,
+            ]
         );
     }
-    public function setPending($id,$jid){
-        $sql = "UPDATE  messages_pending set `pending` = 1, `jid` = :jid where `id` = :id";
+
+    public function setPending($id, $jid)
+    {
+        $sql = 'UPDATE  messages_pending set `pending` = 1, `jid` = :jid where `id` = :id';
         $query = $this->db->prepare($sql);
         $query->execute(
-            array(
-                    ':id'=>$id,
-                    ':jid'=>$jid
-                )
+            [
+                    ':id'  => $id,
+                    ':jid' => $jid,
+                ]
         );
-        $sql = "INSERT OR IGNORE into messages_pending(`id`,`jid`, `pending`) VALUES(:id,:jid,1)";
+        $sql = 'INSERT OR IGNORE into messages_pending(`id`,`jid`, `pending`) VALUES(:id,:jid,1)';
         $query = $this->db->prepare($sql);
         $query->execute(
-            array(
-                    ':id'=>$id,
-                    ':jid'=>$jid
-                )
+            [
+                    ':id'  => $id,
+                    ':jid' => $jid,
+                ]
         );
     }
-    public function getPending($jid){
-        $sql = "SELECT `id` from messages_pending where `jid` = :jid and `pending` = 1";
+
+    public function getPending($jid)
+    {
+        $sql = 'SELECT `id` from messages_pending where `jid` = :jid and `pending` = 1';
         $query = $this->db->prepare($sql);
         $query->execute(
-            array(
-                ':jid'=>$jid
-            )
+            [
+                ':jid' => $jid,
+            ]
         );
         $pending_ids = [];
-        while($row = $query->fetch(PDO::FETCH_ASSOC))
-        {
-            if($row != null && $row !== false)
-            {
-               $pending_ids[] = $row['id'];
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            if ($row != null && $row !== false) {
+                $pending_ids[] = $row['id'];
             }
-
         }
-        if(count($pending_ids) == 0) return [];
+        if (count($pending_ids) == 0) {
+            return [];
+        }
         $messages = [];
-        $qMarks = str_repeat('?,', count($pending_ids) - 1) . '?';
+        $qMarks = str_repeat('?,', count($pending_ids) - 1).'?';
         $sql = "SELECT * from messages where `id` IN ($qMarks)";
         $query = $this->db->prepare($sql);
         $query->execute($pending_ids);
-        while($row = $query->fetch(PDO::FETCH_ASSOC))
-        {
-            if($row != null && $row !== false)
-            {
-               $messages[] = $row;
+        while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+            if ($row != null && $row !== false) {
+                $messages[] = $row;
             }
         }
-        $sql = "DELETE FROM messages_pending  where `pending` = 1 and jid = :jid";
+        $sql = 'DELETE FROM messages_pending  where `pending` = 1 and jid = :jid';
         $query = $this->db->prepare($sql);
-        $query->execute(array(':jid'=>$jid));
+        $query->execute([':jid' => $jid]);
+
         return $messages;
     }
 }
-
-
