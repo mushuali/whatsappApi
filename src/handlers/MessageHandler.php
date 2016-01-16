93eaa208f80837f5d98f349dcf614da8e668789a
mgp25 <me@mgp25.com>
2016-01-16 08:51:17 +0100

    fixes #1490

--- src/handlers/MessageHandler.php
+++ src/handlers/MessageHandler.php
@@ -38,7 +38,7 @@ class MessageHandler implements Handler
             $this->processMessageNode($this->node);
         }
         if ($this->node->getAttribute('type') == 'media' && ($this->node->getChild('media') != null || $this->node->getChild('enc') != null)) {
-          $file_data = "";  
+          $file_data = "";
 	  if ($this->node->getChild('enc') != null && $this->node->getAttribute('participant') == null) { // for now only private messages
 
                 if (extension_loaded('curve25519') && extension_loaded('protobuf')) {
@@ -50,10 +50,10 @@ class MessageHandler implements Handler
                     	$file_data = $dec_node->getChild("media")->getAttribute("file");
                 }
             }
-            else if ($this->node->getChild("enc") == null) {
+            else if (($this->node->getChild("enc") == null) && ($this->node->getChild('media')->getAttribute('url') != null)) {
               $file_data = file_get_contents($this->node->getChild('media')->getAttribute('url'));
             }
-	
+
             if ($this->node->getChild('media') != null) {
                 if ($this->node->getChild('media')->getAttribute('type') == 'image') {
                     if ($this->node->getAttribute('participant') == null) {
